1190b0ad79c380195f4ec214224d08d6ea8cfa95
도경 <dokyungkim2926@gmail.com>
2025-03-13 08:31:25 +0900

    Auto stash before checking out "origin/main"

--- src/main/java/com/example/ouath/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/ouath/domain/user/dao/UserRepository.java
@@ -9,8 +9,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    @Query(value = "SELECT u.id, u.account_id, u.birth_day, u.class_num, u.club, u.device_token, u.entrance_year, u.grade, u.name, u.num, u.password, u.profile_file_name, u.role, u.state " +
-            "FROM tbl_user u WHERE u.account_id LIKE CONCAT('%', :accountId, '%')", nativeQuery = true)
+    @Query(value = "SELECT u.id, u.account_id, u.birth_day, u.class_num, u.club, u.device_token, " +
+            "u.entrance_year, u.grade, u.name, u.num, u.password, u.profile_file_name, u.role, u.state " +
+            "FROM tbl_user u WHERE u.account_id = :accountId", nativeQuery = true)
     Optional<User> findByAccountId(@Param("accountId") String accountId);
     Boolean existsByGradeAndClassNumAndNum(Integer grade, Integer classNum, Integer num);
     Boolean existsByAccountId(String accountId);
